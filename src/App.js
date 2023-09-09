eef176266f51a204a49ea2339fd7335758e5ee7b
Rahul R <127472805+Rahul-R-1796@users.noreply.github.com>
2023-09-09 18:26:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,58 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import PostCard from './PostCard';
-import SearchBar from './SearchBar';
-import DeleteButton from './DeleteButton';
+// src/App.tsx
+import React from 'react';
+import { Container, CssBaseline, Typography } from "@mui/material";
+import PostList from './components/PostList';
 
 function App() {
-  const [posts, setPosts] = useState([]);
-  const [filteredPosts, setFilteredPosts] = useState([]);
-  const [searchQuery, setSearchQuery] = useState('');
-
-  useEffect(() => {
-    // Check if posts are stored in localStorage
-    const storedPosts = localStorage.getItem('posts');
-
-    if (storedPosts) {
-      // If posts are available in localStorage, parse and set them in state
-      setPosts(JSON.parse(storedPosts));
-      setFilteredPosts(JSON.parse(storedPosts));
-    } else {
-      // Fetch posts from the API on initial load
-      axios.get('https://jsonplaceholder.typicode.com/posts')
-        .then(response => {
-          const fetchedPosts = response.data;
-          // Store fetched posts in localStorage
-          localStorage.setItem('posts', JSON.stringify(fetchedPosts));
-          setPosts(fetchedPosts);
-          setFilteredPosts(fetchedPosts);
-        })
-        .catch(error => {
-          console.error('Error fetching posts:', error);
-        });
-    }
-  }, []);
-
-  // Handle search query changes and filter posts
-  useEffect(() => {
-    const filtered = posts.filter(post =>
-      post.title.toLowerCase().includes(searchQuery.toLowerCase())
-    );
-    setFilteredPosts(filtered);
-  }, [searchQuery, posts]);
-
   return (
-    <div>
-      <SearchBar setSearchQuery={setSearchQuery} />
-      <div className="post-list">
-        {filteredPosts.map(post => (
-          <div key={post.id}>
-            <PostCard post={post} />
-            <DeleteButton postId={post.id} />
-          </div>
-        ))}
-      </div>
-    </div>
+    <Container maxWidth="md">
+      <CssBaseline /> 
+      <Typography variant="h4" align="center" gutterBottom>
+        Posts and Comments
+      </Typography>
+      <PostList /> 
+    </Container>
   );
 }
 
