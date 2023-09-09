471128d0402548085108a5b25eec5d24db5f1860
Rahul R <127472805+Rahul-R-1796@users.noreply.github.com>
2023-09-09 18:50:55 +0530

    Update PostCard.js

--- src/PostCard.js
+++ src/PostCard.js
@@ -1,33 +1,45 @@
-import React, { useState } from 'react';
-import { Card, CardContent, Typography, Button, Dialog } from '@mui/material';
-import PostDialog from './PostDialog';
-import DeleteButton from './DeleteButton';
+// PostCard.js
+import React, { useState, useEffect } from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
 
-function PostCard({ post }) {
-  const [dialogOpen, setDialogOpen] = useState(false);
+const PostCard = () => {
+  const [posts, setPosts] = useState([]);
 
-  const handleDialogOpen = () => {
-    setDialogOpen(true);
-  };
-
-  const handleDialogClose = () => {
-    setDialogOpen(false);
-  };
+  useEffect(() => {
+    // Check if posts are already in local storage
+    const storedPosts = JSON.parse(localStorage.getItem('posts'));
+    
+    if (storedPosts) {
+      setPosts(storedPosts);
+    } else {
+      // Fetch posts from the API on the first load
+      fetch('https://jsonplaceholder.typicode.com/posts')
+        .then((response) => response.json())
+        .then((data) => {
+          setPosts(data);
+          // Save posts to local storage
+          localStorage.setItem('posts', JSON.stringify(data));
+        })
+        .catch((error) => console.error(error));
+    }
+  }, []);
 
   return (
-    <Card className="post-card">
-      <CardContent>
-        <Typography variant="h5" component="div">
-          {post.title}
-        </Typography>
-        <Button onClick={handleDialogOpen}>View Comments</Button>
-        <DeleteButton postId={post.id} />
-      </CardContent>
-      <Dialog open={dialogOpen} onClose={handleDialogClose}>
-        <PostDialog postId={post.id} onClose={handleDialogClose} />
-      </Dialog>
-    </Card>
+    <div>
+      {posts.map((post) => (
+        <Card key={post.id} variant="outlined" style={{ margin: '16px' }}>
+          <CardContent>
+            <Typography variant="h5" component="div">
+              {post.title}
+            </Typography>
+            <Typography color="text.secondary">{post.body}</Typography>
+          </CardContent>
+        </Card>
+      ))}
+    </div>
   );
-}
+};
 
 export default PostCard;
